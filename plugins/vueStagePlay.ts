f1e64456aa5c0166cdc94a17ef7f209461f0f84d
max.lee <f820602h@gmail.com>
2023-11-16 23:18:31 +0800

    [chore] update vue-stage-play options

--- plugins/vueStagePlay.ts
+++ plugins/vueStagePlay.ts
@@ -1,5 +1,10 @@
 import { stagePlayPlugin } from "vue-stage-play";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(stagePlayPlugin());
+  nuxtApp.vueApp.use(
+    stagePlayPlugin({
+      cameraFollowOffset: 70,
+      cameraFixAfterFollow: false,
+    }),
+  );
 });
